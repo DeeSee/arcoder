cb5b18e2d0b42de6553843202ea1572de4b0a075
DeeSee <sas.moscow@gmail.com>
2012-09-08 18:09:26 +0400

    Some refactoring

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 #include "Arcoder.h"
 
 
-#define PRINT_USAGE_AND_EXIT PrintUsage(argv[0]); return 1;
-
 struct ArcoderSettings
 {
 	bool compress;
@@ -31,21 +29,23 @@ void PrintUsage(const char* i_pathToProgram)
   std::cout << "Usage: " << i_pathToProgram << " -e|d -i /path/to/input -o /path/to/output [-m /path/to/model] [-c /path/to/model/choosing/table]" << std::endl;
 }
 
-int ParseOptions(int argc, char** argv, ArcoderSettings& settings)
+int ParseOptions(int i_argc, const char** i_argv, ArcoderSettings& o_settings)
 {
 	int i = 1;
 	bool compressOptionIsSet = false;
+
+  o_settings = ArcoderSettings();
 	
-	while (i < argc)
+	while (i < i_argc)
 	{
 		const int kOptionLength = 2;
-		if (argv[i][0] != '-' || strlen(argv[i]) != kOptionLength)
+		if (i_argv[i][0] != '-' || strlen(i_argv[i]) != kOptionLength)
 		{
-			std::cout << "Error: unknown option " << argv[i] << std::endl;
+			std::cout << "Error: unknown option " << i_argv[i] << std::endl;
 			return 1;
 		}
 
-		if ((argv[i][1] == 'e') || (argv[i][1] == 'd'))
+		if ((i_argv[i][1] == 'e') || (i_argv[i][1] == 'd'))
 		{
 			if (compressOptionIsSet)
 			{
@@ -54,31 +54,31 @@ int ParseOptions(int argc, char** argv, ArcoderSettings& settings)
 			}
 
 			compressOptionIsSet = true;
-			settings.compress = (argv[i][1] == 'e');
+			o_settings.compress = (i_argv[i][1] == 'e');
 		} 
 		else // all other options must have argument
 		{
-			if (i + 1 == argc)
+			if (i + 1 == i_argc)
 			{
-				std::cout << "Error: option " << argv[i] << " must have an argument" << std::endl;
+				std::cout << "Error: option " << i_argv[i] << " must have an argument" << std::endl;
 				return 1;
 			}
 
-			if (argv[i][1] == 'i')
+			if (i_argv[i][1] == 'i')
 			{
-				settings.inputFileName = std::string(argv[i + 1]);
+				o_settings.inputFileName = std::string(i_argv[i + 1]);
 			}
-			else if (argv[i][1] == 'o')
+			else if (i_argv[i][1] == 'o')
 			{
-				settings.outputFileName = std::string(argv[i + 1]);
+				o_settings.outputFileName = std::string(i_argv[i + 1]);
 			}
-			else if (argv[i][1] == 'm')
+			else if (i_argv[i][1] == 'm')
 			{
-				settings.modelFileName = std::string(argv[i + 1]);
+				o_settings.modelFileName = std::string(i_argv[i + 1]);
 			}
-			else if (argv[i][1] == 'c')
+			else if (i_argv[i][1] == 'c')
 			{
-				settings.modelFileName = std::string(argv[i + 1]);
+				o_settings.modelFileName = std::string(i_argv[i + 1]);
 			}
 
 			i++;
@@ -87,13 +87,13 @@ int ParseOptions(int argc, char** argv, ArcoderSettings& settings)
 		i++;
 	}
 
-	if (settings.inputFileName.empty())
+	if (o_settings.inputFileName.empty())
 	{
 		std::cout << "Input file name must be specified!" << std::endl;
 		return 1;
 	}
 
-	if (settings.outputFileName.empty())
+	if (o_settings.outputFileName.empty())
 	{
 		std::cout << "Output file name must be specified!" << std::endl;
 		return 1;
@@ -106,10 +106,11 @@ int main(int argc, char** argv)
 {
   ArcoderSettings settings;
 
-  if (ParseOptions(argc, argv, settings) != 0)
+  if (ParseOptions(argc, (const char **)argv, settings) != 0)
   {
 	  std::cout << "Failed to parse options." << std::endl;
-	  PRINT_USAGE_AND_EXIT;
+	  PrintUsage(argv[0]);
+    return 1;
   }
 
   std::ifstream in(settings.inputFileName.c_str());
