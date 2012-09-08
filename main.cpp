b470d319511d6f951e229f83c59b758da1cc08d5
DeeSee <sas.moscow@gmail.com>
2012-09-08 17:35:06 +0400

    Added options parsing (model file not tested yet, model choosing table is unused)

--- main.cpp
+++ main.cpp
@@ -1,57 +1,153 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <assert.h>
 
 #include "Arcoder.h"
 
 
 #define PRINT_USAGE_AND_EXIT PrintUsage(argv[0]); return 1;
 
+struct ArcoderSettings
+{
+	bool compress;
+	std::string inputFileName;
+	std::string outputFileName;
+	std::string modelFileName;
+	std::string modelChoosingRulesFileName;
+
+	ArcoderSettings()
+	{
+		compress = true;
+		inputFileName = std::string();
+		outputFileName = std::string();
+		inputFileName = std::string();
+		modelChoosingRulesFileName = std::string();
+	}
+};
+
 void PrintUsage(const char* i_pathToProgram)
 {
-  std::cout << "Usage: " << i_pathToProgram << " [e|d] /path/to/input /path/to/output" << std::endl;
+  std::cout << "Usage: " << i_pathToProgram << " -e|d -i /path/to/input -o /path/to/output [-m /path/to/model] [-c /path/to/model/choosing/table]" << std::endl;
 }
 
+int ParseOptions(int argc, char** argv, ArcoderSettings& settings)
+{
+	int i = 1;
+	bool compressOptionIsSet = false;
+	
+	while (i < argc)
+	{
+		const int kOptionLength = 2;
+		if (argv[i][0] != '-' || strlen(argv[i]) != kOptionLength)
+		{
+			std::cout << "Error: unknown option " << argv[i] << std::endl;
+			return 1;
+		}
+
+		if ((argv[i][1] == 'e') || (argv[i][1] == 'd'))
+		{
+			if (compressOptionIsSet)
+			{
+				std::cout << "Error: -e and -d options must not be presented simultaniously or more than once!" << std::endl;
+				return 1;
+			}
+
+			compressOptionIsSet = true;
+			settings.compress = (argv[i][1] == 'e');
+		} 
+		else // all other options must have argument
+		{
+			if (i + 1 == argc)
+			{
+				std::cout << "Error: option " << argv[i] << " must have an argument" << std::endl;
+				return 1;
+			}
+
+			if (argv[i][1] == 'i')
+			{
+				settings.inputFileName = std::string(argv[i + 1]);
+			}
+			else if (argv[i][1] == 'o')
+			{
+				settings.outputFileName = std::string(argv[i + 1]);
+			}
+			else if (argv[i][1] == 'm')
+			{
+				settings.modelFileName = std::string(argv[i + 1]);
+			}
+			else if (argv[i][1] == 'c')
+			{
+				settings.modelFileName = std::string(argv[i + 1]);
+			}
+
+			i++;
+		}
+
+		i++;
+	}
+
+	if (settings.inputFileName.empty())
+	{
+		std::cout << "Input file name must be specified!" << std::endl;
+		return 1;
+	}
+
+	if (settings.outputFileName.empty())
+	{
+		std::cout << "Output file name must be specified!" << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
 
 int main(int argc, char** argv)
 {
-  if (argc != 4)
+  ArcoderSettings settings;
+
+  if (ParseOptions(argc, argv, settings) != 0)
   {
-    PRINT_USAGE_AND_EXIT;
+	  std::cout << "Failed to parse options." << std::endl;
+	  PRINT_USAGE_AND_EXIT;
   }
 
-  bool compress = true;
-
-  if (!strcmp(argv[1], "e"))
-  {
-    compress = true;
-  }
-  else if (!strcmp(argv[1], "d"))
-  {
-    compress = false;
-  }
-  else
-  {
-    PRINT_USAGE_AND_EXIT;
-  }
-
-  std::ifstream in(argv[2]);
-  std::ofstream out(argv[3]);
+  std::ifstream in(settings.inputFileName.c_str());
+  std::ofstream out(settings.outputFileName.c_str());
 
   if (!in.good())
   {
-    std::cout << "Couldn't open " << argv[2] << " for reading" << std::endl;
+    std::cout << "Couldn't open " << settings.inputFileName.c_str() << " for reading" << std::endl;
     return 2;
   }
 
   if (!out.good())
   {
-    std::cout << "Couldn't open " << argv[3] << " for writing" << std::endl;
+    std::cout << "Couldn't open " << settings.outputFileName.c_str() << " for writing" << std::endl;
+    return 2;
   }
 
-  Arcoder* coder = CreateArcoder();
+  Arcoder* coder = NULL;
+  
+  if (!settings.modelFileName.empty())
+  {
+    std::ifstream model(settings.modelFileName.c_str());
+    if (!model.good())
+    {
+      std::cout << "Couldn't open " << settings.modelFileName.c_str() << " for reading" << std::endl;
+      return 2;
+    }
 
-  if (compress)
+		coder = CreateArcoder(model);
+  }
+  else
+  {
+    coder = CreateArcoder();
+  }
+
+  assert(coder != NULL);
+
+  if (settings.compress)
   {
     coder->Compress(in, out);
   }
